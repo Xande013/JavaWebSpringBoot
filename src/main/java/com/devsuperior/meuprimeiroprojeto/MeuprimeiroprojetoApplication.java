68510f86cca32adae93b04d1045046b4c51a0154
Xande013 <alexandremrp@hotmail.com>
2022-03-17 20:51:03 -0300

    Category memory

---
+++ src/main/java/com/devsuperior/meuprimeiroprojeto/MeuprimeiroprojetoApplication.java
@@ -0,0 +1,33 @@
+package com.devsuperior.meuprimeiroprojeto;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.meuprimeiroprojeto.entities.Category;
+import com.devsuperior.meuprimeiroprojeto.repositories.CategoryRepository;
+
+
+@SpringBootApplication
+public class MeuprimeiroprojetoApplication implements CommandLineRunner {
+
+	@Autowired 
+	private CategoryRepository categoryRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MeuprimeiroprojetoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+		
+	}
+
+}
